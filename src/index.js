74c004c76c58eafb4f06f5ebb83d294ff08b1432
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 12:26:31 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var reInflect = require("re_inflect");
 
 
-module.exports = function underscore(string) {
+module.exports = underscore;
+
+
+function underscore(string) {
     return string.match(reInflect).join("_").toLowerCase();
-};
+}
